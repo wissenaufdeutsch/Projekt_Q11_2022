663fc062a87a359c6966dbf61c0508a758932669
Stefanie <stefanie.auerswald@web.de>
2022-05-18 09:13:59 +0200

    abstrakte Klasse LEVEL erstellt

---
+++ LEVEL.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Abstrakte Klasse LEVEL - Enthält alle wichtigen Methoden, die für die Erstellung von Levels nötig sind
+ * 
+ * @author (Stefanie)
+ * @version (18.05.2022)
+ */
+public abstract class LEVEL
+{
+    ArrayList <OBSTACLE> obstacles;
+    
+    
+}
