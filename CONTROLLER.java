35e5b141e199e7c81c305f2bbb18a8dc844fe060
Andreas Mühlmann <andi.muehlmann@arcor.de>
2022-05-18 09:32:54 +0200

    more basic stuff

--- CONTROLLER.java
+++ CONTROLLER.java
@@ -10,6 +10,14 @@ public class CONTROLLER
     public CONTROLLER(String mode) {
         this.mode = mode;
         this.done = false;
+        LEVEL level = chooseLevel();
+        //create env
+        
+    }
+    
+    public LEVEL chooseLevel() {
+        //choose a Level (whatch mode)
+        return null;
     }
     
     public void run() {
