bf230260ee1fed60a6ad1f1b621b60d78b49bc4e
TassiLOL <tassilo.stadler@web.de>
2022-05-18 09:12:02 +0200

    Spielregeln zu TicTacToe eingefuegt  (ungestestet)

---
+++ KÄSTCHEN.java
@@ -0,0 +1,22 @@
+
+public class KÄSTCHEN
+{
+    String belegt;
+    KÄSTCHEN(){
+        belegt=null; //null:nicht belegt, "Kreuz": Kreuz, "Kreis": Kreis
+    }
+
+    void Belegen(String s){
+        if(belegt!=null){
+            belegt=s; 
+
+        } else {
+            System.out.println("Schon belegt!");
+        }
+    }
+
+    String belegtGeben(){
+        return belegt;
+    }
+}
+
