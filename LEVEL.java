2af4139210ae9f3ed19ca0d50e4c1cfcdbebd1ed
Stefanie <stefanie.auerswald@web.de>
2022-05-19 14:45:35 +0200

    Rechteck entsteht in LEVEL

--- LEVEL.java
+++ LEVEL.java
@@ -3,15 +3,20 @@ public class LEVEL
 {
     private OBSTACLE [][] obstacles;
     private int ende;
+    
     public LEVEL()
     {
         obstacles = new OBSTACLE [1000][1000];
         ende = 0;
+        
+        RECHTECK r = new RECHTECK(10, 10, 0, 0);
     }
     
     public OBSTACLE GiveObstacle(int level, int obstacle)
     {
         return obstacles[level][obstacle];
     }
+    
+
     }
 
