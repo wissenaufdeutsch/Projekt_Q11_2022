1f8e47e7ccf7bd324c08e9db35d4da41ea59cd5d
LuisaTree <photo.luisa.graphy@gmx.de>
2022-05-19 14:44:01 +0200

    Rechteck erstellt

--- OBSERVER.java
+++ OBSERVER.java
@@ -5,5 +5,4 @@ public class OBSERVER implements MESSAGERECIVER
 {
 
 
-
 }
---
+++ RECHTECK.java
@@ -0,0 +1,14 @@
+
+
+public class RECHTECK 
+{ public RECHTECK(int width, int height,int xpos, int ypos)
+    {} 
+
+    void CreateRectangle()
+    {
+        RECHTECK objekt = new RECHTECK(5,5,10,12);
+
+ 
+
+    }
+}
