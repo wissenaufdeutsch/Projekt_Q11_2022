1e7764df0ac508c263d0b8d4108a6c718215a8a7
Kili <Kili.Schreiner@gmail.com>
2022-06-22 08:55:09 +0200

    net

--- Server.java
+++ Server.java
@@ -66,7 +66,7 @@ public class Server extends Thread {
             e1.printStackTrace();//Stack blaaaa: You can do this better!
         }
         int x = 0;
-        while(x!=3 && !shutdown) {//as long as nobody wants to kill the server real badly
+        while(x!=2 && !shutdown) {//as long as nobody wants to kill the server real badly
             try {// Wait for a new connection.
                 System.out.println("im here");//test sysout
                 SocketChannel chan = ssc.accept();//try to accept an incoming connection
@@ -88,6 +88,9 @@ public class Server extends Thread {
                 break;
             }
         }
+        //leererSpielstand
+        
+        System.out.println("Server full");
     }
     
     void sendmessanges(String m){
@@ -121,6 +124,7 @@ public class Server extends Thread {
         public ConnectionThread(TextSocketChannel conn,Server se) {
             this.conn = conn;
             this.s=se;
+            
         }
 
         @Override
